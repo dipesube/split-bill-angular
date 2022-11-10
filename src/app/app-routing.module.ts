1e0e76b4ff2d33645482a338053a601d8986ee43
Dipendra Subedi <dipesube@gmail.com>
2022-11-09 19:08:58 -0500

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CustomerComponent },
+  { path: 'restaurant', component: RestaurantComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
