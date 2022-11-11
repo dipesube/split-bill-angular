1aaa45383cba12cafb922577fb0f5ae55d4ec974
Dipendra Subedi <dipesube@gmail.com>
2022-11-10 22:45:03 -0500

    used mat table

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  price: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', price: 1.79 },
+  { position: 2, name: 'Helium', price: 4.26 },
+  { position: 3, name: 'Lithium', price: 6.91 },
+  { position: 4, name: 'Beryllium', price: 9.22 },
+  { position: 5, name: 'Boron', price: 10.81 },
+  { position: 6, name: 'Carbon', price: 12.01 },
+  { position: 7, name: 'Nitrogen', price: 14.00 },
+  { position: 8, name: 'Oxygen', price: 15.99 },
+  { position: 9, name: 'Fluorine', price: 18.99 },
+  { position: 10, name: 'Neon', price: 20.17 },
+];
+
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.scss']
 })
+
 export class CustomerComponent implements OnInit {
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
+  displayedColumns: string[] = ['position', 'name', 'price'];
+  dataSource = ELEMENT_DATA;
+
   constructor() { }
 
   ngOnInit() {
